6561a351a7cdbdbdc5a28076028d9ae9371aa2c4
Alberto Araújo <albertoafaj@hotmail.com>
2022-10-22 13:45:35 -0300

    Create method to consume API

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const vm = new Vue({
     el: "#app",
     data: {
+        produtos: []
+    },
+    methods: {
+        fetchProdutos() {
+            fetch("./api/produtos.json")
+                .then(r => r.json())
+                .then((json) => this.produtos = json)
+
+
+        }
+    },
+    created() {
+        this.fetchProdutos();
     }
+
 })
